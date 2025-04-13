fb674c76eb9c75f3ded34e17370ed0038dc303ba
Hugo Rosa <194843128+HugoRosa29@users.noreply.github.com>
2025-04-13 16:27:19 -0300

    Update ex059.py

--- ex059.py
+++ ex059.py
@@ -2,8 +2,11 @@ def concatenar(a, b):
     return a + b
 def repetir(a, b):
     return a * b
+def ambos(a, b, num):
+    concatenado = concatenar(a, b)
+    return repetir(concatenado, num)
 
 lista = input().split()
 print(concatenar(lista[0], lista[1]))
 print(repetir(lista[0], int(lista[2])))
-print(repetir(concatenar(lista[0], lista[1]), int(lista[2])))
+print(ambos(lista[0], lista[1], int(lista[2])))
